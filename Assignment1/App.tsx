a9bdf1a9fb83b1e2fa748a92f3e5cd9d3bc6c658
ShirazPirzada <64315910+ShirazPirzada@users.noreply.github.com>
2023-06-02 00:05:59 +0500

    Add files via upload

---
+++ Assignment1/App.tsx
@@ -0,0 +1,89 @@
+import {useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+function App() {
+  const [userName, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const _login = () => {
+    console.log({userName, password});
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerView}>
+        <Text style={styles.login}>Login</Text>
+        <Text style={styles.subTitle}>Welcome To DSU Login</Text>
+      </View>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Enter User Name"
+        placeholderTextColor={'white'}
+        value={userName}
+        onChangeText={value => setUserName(userName)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Password"
+        placeholderTextColor={'white'}
+        value={password}
+        onChangeText={value => setPassword(password)}
+        secureTextEntry
+      />
+
+      <TouchableOpacity onPress={() => _login()} style={styles.button}>
+        <Text>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    // justifyContent:'',
+    //alignItems:'center'
+  },
+
+  button: {
+    backgroundColor: 'tomato',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginTop: 25,
+  },
+  headerView: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  input: {
+    height: 50,
+    paddingHorizontal: 15,
+    color: 'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    marginTop: 15,
+  },
+  login: {
+    color: 'white',
+    fontSize: 25,
+    // textAlign:'center',
+  },
+  subTitle: {
+    color: 'white',
+    fontSize: 25,
+  },
+});
+
+export default App;
